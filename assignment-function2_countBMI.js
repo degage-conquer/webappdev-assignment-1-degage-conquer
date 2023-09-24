5fea9fab06c38cd452da84e636f62845fe35c9c2
unknown <zxm18974825@qq.com>
2023-09-24 17:24:34 +0800

    函数作业

---
+++ assignment-function2_countBMI.js
@@ -0,0 +1,9 @@
+//多参数，无return
+//输入身高，体重，计算bmi
+function count_bmi(weight,height){
+    weight=weight;
+    height=height;
+    let bmi=weight/(height^2)
+    console.log(bmi)
+}
+count_bmi(50,1.6);//16.666
